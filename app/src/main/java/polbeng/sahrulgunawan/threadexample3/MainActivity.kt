5d9fad88dd7dd4f094e58293f3706dfb194b16f7
LENOVO <sahrulcc07@gmail.com>
2024-10-29 19:45:12 +0700

    Commit Thread 3

---
+++ app/src/main/java/polbeng/sahrulgunawan/threadexample3/MainActivity.kt
@@ -0,0 +1,21 @@
+package polbeng.sahrulgunawan.threadexample3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import polbeng.sahrulgunawan.threadexample3.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnStartThread.setOnClickListener {
+            startThread()
+        }
+    }
+    private fun startThread(){
+        val runnable = ExampleRunnable(10);
+        Thread(runnable).start()
+    }
+}
